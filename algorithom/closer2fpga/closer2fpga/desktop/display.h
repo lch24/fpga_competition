c6ce09a8a7f8cdf4818b508a2896f284c811b27d
Ilexshadow <chensu433@outlook.com>
2026-09-23 16:24:52 +0800

    add OV5640 dual-view FPGA project (RTL/IP/constraints, source only)

--- algorithom/closer2fpga/closer2fpga/desktop/display.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../algo/chessboard.h"
-#include <opencv2/core.hpp>
-namespace desktop {
-cv::Mat draw_corners(const cv::Mat& image, const ChessboardInfo& board);
-cv::Mat comparison(const cv::Mat& original, const GrayImage& corrected);
-} // namespace desktop
